12db8901bce684e8abfcc6b4ca76f1305f3e6398
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-11-30 15:03:44 +0530

    add new resoure for the garage api endpoint User add

--- backend/apps/garage-api/src/garage-api.module.ts
+++ backend/apps/garage-api/src/garage-api.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GarageApiController } from './garage-api.controller';
 import { GarageApiService } from './garage-api.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [GarageApiController],
   providers: [GarageApiService],
 })
