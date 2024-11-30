012c62c48d26c2b079d2585b87b6e2f6303fb411
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-11-30 14:28:46 +0530

    add new declaration

--- backend/apps/garage-api/src/garage-api.service.ts
+++ backend/apps/garage-api/src/garage-api.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class GarageApiService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! from Garage API';
   }
 }
