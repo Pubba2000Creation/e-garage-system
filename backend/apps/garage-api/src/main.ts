e6296272b9de83bb8fd7eb777bf16ab295137f7d
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-11-30 14:19:14 +0530

    setup for monorepo

--- backend/apps/garage-api/src/main.ts
+++ backend/apps/garage-api/src/main.ts
@@ -3,6 +3,6 @@ import { GarageApiModule } from './garage-api.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GarageApiModule);
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();
