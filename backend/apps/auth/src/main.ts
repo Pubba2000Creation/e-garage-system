a8724e7bc1ddb5f7e0f4d71ba2b85615e848bdbe
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-12-02 23:33:25 +0530

    Use JWT for Access and Refresh Tokens:
        Install @nestjs/jwt and bcrypt:
    
    pnpm add @nestjs/jwt @nestjs/passport bcrypt

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3001);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('API documentation for authentication')
+    .setVersion('1.0')
+    .addTag('auth') // Optional, helps group endpoints
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3001); // App will run on port 3001
 }
 bootstrap();
