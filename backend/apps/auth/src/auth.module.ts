63d3916e250af800cb5dd1a0ec4ebd57c9d25bfe
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-12-24 20:45:42 +0530

    update auth module base on the uploder module

--- backend/apps/auth/src/auth.module.ts
+++ backend/apps/auth/src/auth.module.ts
@@ -5,10 +5,12 @@ import { UserModule } from './user/user.module';
 import { DatabaseModule } from '@app/common/database/database.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { S3Module } from '@app/common';
 
 @Module({
   imports: [
     ConfigModule,
+    S3Module,
     UserModule,
     DatabaseModule,
     JwtModule.registerAsync({
