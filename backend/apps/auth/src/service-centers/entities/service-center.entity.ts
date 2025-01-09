c2d0cc87468f65b5e2697c00fcf37776ccad0cdf
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2025-01-09 16:50:32 +0530

    create new repository and update module for the mongo DB enviroment

--- backend/apps/auth/src/service-centers/entities/service-center.entity.ts
+++ backend/apps/auth/src/service-centers/entities/service-center.entity.ts
@@ -1,1 +1,9 @@
-export class ServiceCenter {}
+import { AbstractDocument } from "@app/common";
+import { SchemaFactory } from "@nestjs/mongoose";
+
+export class ServiceCenterDocument extends AbstractDocument {
+    
+}
+
+
+export const ServiceCenterSchema = SchemaFactory.createForClass(ServiceCenterDocument)
