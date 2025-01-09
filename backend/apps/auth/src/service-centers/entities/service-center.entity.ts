c412e7cf56356efb7b620b8e31ba4bc9b2f1df2b
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2025-01-09 23:59:27 +0530

    change user entityies and create some ser cener beased enum values

--- backend/apps/auth/src/service-centers/entities/service-center.entity.ts
+++ backend/apps/auth/src/service-centers/entities/service-center.entity.ts
@@ -1,8 +1,55 @@
 import { AbstractDocument } from "@app/common";
-import { SchemaFactory } from "@nestjs/mongoose";
+import { Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Schema } from "@nestjs/mongoose";
+import { ServiceCenterStatus } from "../enum/ServiceCenterStatus.enum";
 
+@Schema({ versionKey: false, timestamps: true})
 export class ServiceCenterDocument extends AbstractDocument {
-    
+
+    @Prop({ type: String, required: true, unique: true })
+    userId: string;
+
+    @Prop({ type: String, required: true })
+    serviceTitle: string;
+
+    @Prop({ type: [String], required: true })
+    serviceCategories: string[];
+
+    @Prop({ type: String, required: true })
+    description: string;
+
+    @Prop({ type: [String], required: false, default: [] })
+    specializedVehicles: string[];
+
+    @Prop({ type: String, required: true })
+    mobileNumber: string;
+
+    @Prop({ type: String, required: false })
+    telephoneNumber?: string;
+
+    @Prop({ type: String, required: true })
+    locationURL: string;
+
+    @Prop({ type: [String], required: false, default: [] })
+    imageGallery?: string[];
+
+    @Prop({ type: String, required: true })
+    ownerName: string;
+
+    @Prop({ type: String, required: true })
+    ownerNIC: string;
+
+    @Prop({ type: String, required: true })
+    businessRegistrationCertificate: string;
+
+    @Prop({
+        type: String,
+        enum: ServiceCenterStatus,
+        required: true,
+        default: ServiceCenterStatus.PENDING,
+    })
+    status: ServiceCenterStatus;
+
 }
 
 
