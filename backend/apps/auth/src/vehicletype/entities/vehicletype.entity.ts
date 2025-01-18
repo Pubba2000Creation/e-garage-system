9989fa46aefc2709825381fb555d7c552eb8d5d9
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2025-01-19 02:23:14 +0530

    add some changers and update json file and build commnds

--- backend/apps/auth/src/vehicletype/entities/vehicletype.entity.ts
+++ backend/apps/auth/src/vehicletype/entities/vehicletype.entity.ts
@@ -1,1 +1,9 @@
-export class Vehicletype {}
+import { AbstractDocument } from "@app/common";
+import { SchemaFactory } from "@nestjs/mongoose";
+
+export class VehicletypeDocument extends AbstractDocument {
+    name:string
+    description:string
+}
+
+export const VehicletypeSchema = SchemaFactory.createForClass(VehicletypeDocument);
---
+++ backend/apps/auth/src/vehicletype/vehicletype.repository.ts
@@ -0,0 +1,4 @@
+import { AbstractRepository } from "@app/common";
+import { VehicletypeDocument } from "./entities/vehicletype.entity";
+
+export class VehicletypeRepository extends AbstractRepository<VehicletypeDocument> {}
