9fe4f398811835b849fc02d53cd60fe79b83522f
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2025-01-21 00:02:56 +0530

    change service-category shecma and vehicle type schema to the system

--- backend/apps/auth/src/vehicletype/entities/vehicletype.entity.ts
+++ backend/apps/auth/src/vehicletype/entities/vehicletype.entity.ts
@@ -1,9 +1,25 @@
 import { AbstractDocument } from "@app/common";
-import { SchemaFactory } from "@nestjs/mongoose";
+import { Prop, SchemaFactory } from "@nestjs/mongoose";
 
 export class VehicletypeDocument extends AbstractDocument {
-    name:string
-    description:string
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: false })
+    category: string;
+
+    @Prop()
+    description: string;
+
+    @Prop({ required: true, min: 2 })
+    wheels: number;
+
+    @Prop({ type: [String], required: true })
+    fuelType: string[];
+
+    @Prop({ required: true, type: Boolean, default: true })
+    isActive: boolean;
+
 }
 
 export const VehicletypeSchema = SchemaFactory.createForClass(VehicletypeDocument);
